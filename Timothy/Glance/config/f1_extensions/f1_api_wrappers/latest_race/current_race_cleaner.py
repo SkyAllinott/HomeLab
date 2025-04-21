70735497736b21a0d146fd60cf133129d901349d
SkyAllinott <skyallinott@gmail.com>
2025-04-21 10:26:46 -0600

    Corrected API link, shortened race name, removing first name fastest lap holder

--- Timothy/Glance/config/f1_extensions/f1_api_wrappers/latest_race/current_race_cleaner.py
+++ Timothy/Glance/config/f1_extensions/f1_api_wrappers/latest_race/current_race_cleaner.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import pytz
 
 app = FastAPI()
-MT = pytz.timezone("America/Denver")
+MT = pytz.timezone("America/Edmonton")
 UTC = pytz.utc
 
 def convert_to_mt(date_str, time_str):
@@ -19,9 +19,19 @@ def convert_to_mt(date_str, time_str):
         dt_mt.isoformat()            # rfc3339 datetime
     )
 
+# Used to fix formatting issue in lap time
+def replace_second_colon(text):
+    first_colon_index = text.find(':')
+    if first_colon_index == -1:
+        return text
+    second_colon_index = text.find(':', first_colon_index + 1)
+    if second_colon_index == -1:
+        return text
+    return (text[:second_colon_index] + '.' + text[second_colon_index + 1:])
+
 @app.get("/f1/last")
 def get_last_race():
-    r = requests.get("https://f1api.dev/api/current/last")
+    r = requests.get("https://f1api.dev/api/current/next")
     data = r.json()
 
     for race in data.get("race", []):
@@ -34,6 +44,12 @@ def get_last_race():
                 val["time"] = time
                 val["datetime_rfc3339"] = rfc
 
+        race_name = race.get("raceName")
+        if race_name:
+            year = data.get("season")
+            year_string = str(year)
+            race['raceName'] = race['raceName'].replace(year_string, "").strip()
+
         # Fix circuit length from meters to kilometers
         circuit = race.get("circuit", {})
         if "circuitLength" in circuit:
@@ -43,10 +59,20 @@ def get_last_race():
             except Exception:
                 circuit["circuitLengthKm"] = None
 
+
         # Capitalize fastest lap driver's last name (if exists)
         fastest_driver_id = circuit.get("fastestLapDriverId")
         if fastest_driver_id:
-            circuit["fastestLapDriverName"] = fastest_driver_id.capitalize()
+            # If only 1 driver in f1 history with last name, 1 last name provided, else full name
+            fastest_driver_clean = fastest_driver_id.replace("_", " ").split(' ')
+            driver_last_name_position = len(fastest_driver_clean)-1
+
+            circuit['fastestLapDriverName'] = fastest_driver_clean[driver_last_name_position].capitalize()
+
+        # European number formatting as "X:YY:ZZZ", replace with "X:YY.ZZZ" NA standard
+        fastest_lap_time = circuit.get("lapRecord")
+        if fastest_lap_time:
+            circuit['lapRecord'] = replace_second_colon(fastest_lap_time)
 
         # Calculate total distance
         laps = race.get("laps")
@@ -56,6 +82,3 @@ def get_last_race():
             race["totalDistanceKm"] = None
 
     return data
-
-
-
